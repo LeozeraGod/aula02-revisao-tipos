d294545717b9320977c040a77a4d2b57f51ab8ef
Leonardo Souza <leonardomellolopes@gmail.com>
2024-11-13 21:33:24 -0300

    novo commit no código

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-console.log("ah lula meu presidenteeee");
+//number ou numérico (inteiros)
+let valor: number = 0;
+
+//Booleano recebe falso ou verdadeiro
+
+let valorCampo: boolean = false;
+
+//String ou cadeia de caracteres
+let valorTexto: string = "essa é uma string";
+
+//Usado para valores indefinidos
+let valorIndefinido = undefined;
+
+//Usado para tipos nulos
+let ValorNulo = null;
+
+const outroValor = 0;
+
+console.log("Mensagem no terminal");
+
+console.log("Olá Mundo");
+
+//Palavras reservadas
+
+if (1) {
+} else {
+    console.log("0");
+}
+
+while (0) {}
+    do {} while (0);
+{
+
+}
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+const chave = 10;
+
+switch (chave) {
+    case 10:
+        console.log("Chave 10");
+        break;
+    default:
+        console.log("Chave Inválida");
+    break;
+}
+
+function teste(valor: string) {
+    return "";
+}
+
+//let novoValor: number;
+let novoValor = 5 + 3;
+// NovoValor = "5"
+
